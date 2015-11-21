70765c7555d583f847aa23c107b57883cc87cbe7
John Huang <johnhuang@kkbox.com>
2015-11-21 19:06:02 +0800

    we missed something

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var io = require('socket.io')(http);
 var listener = io.of('/listener');
 var client = io.of('/client');
 
+var users = {};
+
 app.use(express.static('public'));
 
 http.listen(3000, function(){
@@ -14,14 +16,32 @@ http.listen(3000, function(){
 
 client.on('connection', function(socket){
   console.log('Client connected: ' + socket.id);
-  socket.on('newAlpha', newAlpha);
+  socket.on('beat', beatRecived);
+  socket.on('default', defaultRecived);
+  socket.on('disconnect', function () {
+     var id = this.id;
+     delete users[id];
+     listener.emit('user_update', users);
+
+     console.log('Disconnected: ' + id);
+     console.log('user_update', users);
+  });
 });
 
 listener.on('connection', function(socket){
   console.log('Listener connected: ' + socket.id);
 });
 
-function newAlpha (data) {
-    listener.emit('newAlpha', data)
-    console.log(data);
+function defaultRecived(data) {
+    var id = this.id;
+    users[id] = data;
+    listener.emit('user_update', users);
+
+    console.log('default: ', data);
+    console.log('user_update', users);
+}
+
+function beatRecived (data) {
+    listener.emit('beat', data);
+    console.log('beat: ', data);
 }
